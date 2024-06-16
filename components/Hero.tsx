5f3be68d86a731ca42c9d2d7842e410601d728fc
witekZWitkowic <witoldadamus94@gmail.com>
2024-06-16 14:46:33 +0200

    Button Added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
 
 const Hero = () => {
   return (
@@ -19,6 +22,20 @@ const Hero = () => {
                     <h2 className='uppercase tracking-widest text-xs text-cetner text-blue-100 max-w-80'>
                         Dynamic Web Magic with Next.js
                     </h2>
+                    <TextGenerateEffect 
+                    className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                    words='Transforming Concepts into seamless Experiences'
+                    />
+                    <p className='text-center md:tracking-wider mb-4 text-ms md:text-lg lg:text-2xl'>
+                        Hi! I&apos;m Witold. This is my NextJS page!
+                    </p>
+                    <a href='#about'>
+                        <MagicButton 
+                            title="Show my work"
+                            icon={<FaLocationArrow />}
+                            position='right'
+                        />
+                    </a>
                 </div>
             </div>
     </div>
